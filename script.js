70f3d16433fd5754399f780eddf111a97ec262b9
Kabina Thapa <thapakabina28@gmail.com>
2023-11-28 16:18:22 -0500

    add code to dynamically display notifications from an array of objects

---
+++ script.js
@@ -0,0 +1,37 @@
+
+const notifications=[
+    {
+        name:'Mark',
+    image:'./assets/images/avatar-angela-gray.webp',
+    message:'followed you',
+    time:'3hr ago'
+    },
+    {
+        name:'Joe',
+        image:'./assets/images/avatar-anna-kim.webp',
+        message:'poked you',
+        time:'2hr ago',
+    }
+
+]
+console.log(notifications)
+const containerDiv=document.getElementById('container')
+notifications.map((obj)=>{
+    const container=document.createElement('div')
+    const imgElement=document.createElement('img')
+    imgElement.src=obj.image
+    container.appendChild(imgElement)
+    const nameElement=document.createElement('h1')
+    nameElement.textContent=obj.name
+    container.appendChild(nameElement)
+    const messageElement=document.createElement('div')
+    nameElement.textContent=obj.message
+    container.appendChild(messageElement)
+    const timeElement=document.createElement('h1')
+    nameElement.textContent=obj.time
+    container.appendChild(timeElement)
+    
+containerDiv.appendChild(container)
+
+})
+
