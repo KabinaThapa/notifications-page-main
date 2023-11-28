e4b5c08b83c055b52ef0a35fb6ffaee31560e9c1
Kabina Thapa <thapakabina28@gmail.com>
2023-11-28 16:38:10 -0500

    chore:add classlist to dynamically created elements
    style: apply grid layout

--- script.js
+++ script.js
@@ -11,13 +11,50 @@ const notifications=[
         image:'./assets/images/avatar-anna-kim.webp',
         message:'poked you',
         time:'2hr ago',
-    }
+    },
+    {
+        name:'Mark',
+    image:'./assets/images/avatar-angela-gray.webp',
+    message:'followed you',
+    time:'3hr ago'
+    },
+    {
+        name:'Joe',
+        image:'./assets/images/avatar-anna-kim.webp',
+        message:'poked you',
+        time:'2hr ago',
+    },
+    {
+        name:'Mark',
+    image:'./assets/images/avatar-angela-gray.webp',
+    message:'followed you',
+    time:'3hr ago'
+    },
+    {
+        name:'Joe',
+        image:'./assets/images/avatar-anna-kim.webp',
+        message:'poked you',
+        time:'2hr ago',
+    },
+    {
+        name:'Mark',
+    image:'./assets/images/avatar-angela-gray.webp',
+    message:'followed you',
+    time:'3hr ago'
+    },
+    {
+        name:'Joe',
+        image:'./assets/images/avatar-anna-kim.webp',
+        message:'poked you',
+        time:'2hr ago',
+    },
 
 ]
 console.log(notifications)
 const containerDiv=document.getElementById('container')
 notifications.map((obj)=>{
     const container=document.createElement('div')
+    container.classList.add('notification')
     const imgElement=document.createElement('img')
     imgElement.src=obj.image
     container.appendChild(imgElement)
@@ -25,10 +62,10 @@ notifications.map((obj)=>{
     nameElement.textContent=obj.name
     container.appendChild(nameElement)
     const messageElement=document.createElement('div')
-    nameElement.textContent=obj.message
+    messageElement.textContent=obj.message
     container.appendChild(messageElement)
-    const timeElement=document.createElement('h1')
-    nameElement.textContent=obj.time
+    const timeElement=document.createElement('h4')
+    timeElement.textContent=obj.time
     container.appendChild(timeElement)
     
 containerDiv.appendChild(container)
